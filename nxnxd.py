f056f419d2f30531044ebfc8e086e0db3f5c6b75
Frank Portman <frank1214@gmail.com>
2017-10-29 16:31:08 -0700

    Rework victory checker

--- nxnxd.py
+++ nxnxd.py
@@ -33,80 +33,6 @@ class Game(object):
             print(*[i if i == X or i == O else '  ' for i in row], sep=" |")
             print('----' * self.n)
 
-    # def check_victory(self, row, col):
-    #     '''
-    #     This should short circuit on both the high level directional
-    #     checks as well as inside each directional checker.
-    #     '''
-    #     val = self.board[row][col]
-
-    #     # horizontal score
-    #     score = 1
-    #     # to the right
-    #     for i in range(min(row - self.n, self.d)):
-    #         if self.board[row][col + i + 1] != val:
-    #             break
-    #         score += 1
-    #     # to the left
-    #     for i in range(min(self.n - row, self.d)):
-    #         if self.board[row][col - i - 1] != val:
-    #             break
-    #         score += 1
-
-    #     if score >= self.d:
-    #         return val
-
-    #     # vertical score
-    #     score = 1
-    #     # down
-    #     for i in range(min(col - self.n, self.d)):
-    #         if self.board[row + i + 1][col] != val:
-    #             break
-    #         score += 1
-    #     # up
-    #     for i in range(min(self.n - col, self.d)):
-    #         if self.board[row - i - 1][col] != val:
-    #             break
-    #         score += 1
-
-    #     if score >= self.d:
-    #         return val
-
-    #     # main diagonal
-    #     score = 1
-    #     # down
-    #     for i in range(min(col - self.n, row - self.n, self.d)):
-    #         if self.board[row + i + 1][col + i + 1] != val:
-    #             break
-    #         score += 1
-    #     # up
-    #     for i in range(min(self.n - col, self.n - row, self.d)):
-    #         if self.board[row - i - 1][col - i - 1] != val:
-    #             break
-    #         score += 1
-
-    #     if score >= self.d:
-    #         return val
-
-    #     # anti diagonal
-    #     score = 1
-    #     # down
-    #     for i in range(min(col - self.n, self.n - row, self.d)):
-    #         if self.board[row + i + 1][col - i - 1] != val:
-    #             break
-    #         score += 1
-
-    #     # up
-    #     for i in range(min(self.n - col, row = self.n, self.d)):
-    #         if self.board[row - i - 1][col + i + 1] != val:
-    #             break
-    #         score += 1
-
-    #     if score >= self.d:
-    #         return val
-
-    #     return False
-
     def _direction_checker(self, col_dir=None, row_dir=None):
         '''
         Returns a function that checks a certain row direction or column
@@ -156,27 +82,32 @@ class Game(object):
         return checker
 
     def check_victory(self, row, col):
-        def horizontal_checker(row, col):
-            return 1 + \
-                   self._direction_checker(col_dir=1, row_dir=None)(row, col) + \
-                   self._direction_checker(col_dir=-1, row_dir=None)(row, col)
-        def vertical_checker(row, col):
-            return 1 + \
-                   self._direction_checker(col_dir=None, row_dir=1)(row, col) + \
-                   self._direction_checker(col_dir=None, row_dir=-1)(row, col)
-        def diagonal_checker(row, col):
-            return 1 + \
-                   self._direction_checker(col_dir=1, row_dir=1)(row, col) + \
-                   self._direction_checker(col_dir=-1, row_dir=-1)(row, col)
-        def antidiag_checker(row, col):
-            return 1 + \
-                   self._direction_checker(col_dir=1, row_dir=-1)(row, col) + \
-                   self._direction_checker(col_dir=-1, row_dir=1)(row, col)
+        def make_checker(*args):
+            def checker(row, col):
+                score = 1 
+                for checker in list(args):
+                    score += checker(row, col)
+                    if score >= self.d:
+                        return self.board[row][col]
+                return False
+            return checker
 
-        return horizontal_checker(row, col) == self.d or \
-               vertical_checker(row, col) == self.d   or \
-               diagonal_checker(row, col) == self.d   or \
-               antidiag_checker(row, col) == self.d
+        horizontal_checker = make_checker(self._direction_checker(col_dir=1, row_dir=None),
+                                          self._direction_checker(col_dir=-1, row_dir=None))
+
+        vertical_checker = make_checker(self._direction_checker(col_dir=None, row_dir=1),
+                                        self._direction_checker(col_dir=None, row_dir=-1))
+
+        diagonal_checker = make_checker(self._direction_checker(col_dir=1, row_dir=1),
+                                        self._direction_checker(col_dir=-1, row_dir=-1))
+
+        antidiag_checker = make_checker(self._direction_checker(col_dir=1, row_dir=-1),
+                                        self._direction_checker(col_dir=-1, row_dir=1))
+
+        return horizontal_checker(row, col) or \
+               vertical_checker(row, col)   or \
+               diagonal_checker(row, col)   or \
+               antidiag_checker(row, col)
 
     def move(self, player, row, col):
         self.board[row][col] = player
